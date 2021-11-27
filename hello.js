06be57cc1668b645eabc45e634ec9ff3fe8d3d63
Greg Rodriguez <1965gregory@gmail.com>
2021-11-27 14:50:47 -0500

    Exercices

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
-sayHello("Joe Biden")
+sayHello("Joe Biden");
+
 
 // const sayHello  = function () {
 //   console.log("Hello, world");
---
+++ iceCreamFlavours.js
@@ -0,0 +1,21 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+console.log(iceCreamFlavours.length);
+
+console.log('Bob' || ' Marley');
+
+console.log('Jeremy' > 'Khurram');
+console.log('Don\nBurks');
+
+5 * '3';
+5 + '3';
+5 % '3';
